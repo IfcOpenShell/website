02e5ebcf3b4d2cb3e1df716144e4ed22e154b17c
Dion Moult <dion@thinkmoult.com>
2024-04-06 16:04:29 +1100

    Add popular IFC class search tool again
    It now also supports multi keyword search, predefined type doc search, and multi schema search

--- website/website.py
+++ website/website.py
@@ -5,10 +5,10 @@ import operator
 import requests
 from jinja2 import Environment, FileSystemLoader
 
-osc_apikey = os.environ["OSC_APIKEY"]
-gh_apikey = os.environ["GH_APIKEY"]
 
 def get_contributors():
+    osc_apikey = os.environ["OSC_APIKEY"]
+    gh_apikey = os.environ["GH_APIKEY"]
 
     tier1 = []
     tier2 = []
@@ -38,10 +38,10 @@ def get_contributors():
       }
     }
     """
-    
+
     # @todo We can get the github contribution link from the opencollective profile
     # to sum financial and code contributions.
-    
+
     # @todo Add a little tag on the avator to indicate [$] or [</>] and maybe a tooltip
     # to provide a textual summary of the provided contribution.
 
@@ -50,8 +50,9 @@ def get_contributors():
         results = requests.post(
             endpoint, json={"query": query, "variables": {"slug": slug}}, headers={"Api-Key": osc_apikey}
         ).json()
-        
+
         nodes = results["data"]["account"]["members"]["nodes"]
+
         def make_dict(result):
             slug = result["account"]["slug"]
             di = {
@@ -62,16 +63,16 @@ def get_contributors():
                 "amount": result["totalDonations"]["value"],
             }
             return slug, di
-        
+
         # Seems like some members are mentioned multiple times, with indentical
         # data, folding data based on slug eliminates these duplicates.
         for slug, di in dict(map(make_dict, nodes)).items():
             if slug in dicts:
-                dicts[slug]['amount'] += di['amount']
+                dicts[slug]["amount"] += di["amount"]
             else:
                 dicts[slug] = di
-        
-    for data in sorted(dicts.values(), key=operator.itemgetter('amount'), reverse=True):
+
+    for data in sorted(dicts.values(), key=operator.itemgetter("amount"), reverse=True):
         if data["amount"] >= 500:
             tier1.append(data)
         elif data["amount"] >= 250:
@@ -119,6 +120,7 @@ pages = {
         "blender": "BlenderBIM Add-on - beautiful, detailed, and data-rich OpenBIM",
         "download": "Download - install the BlenderBIM Add-on for Windows, Mac, and Linux",
         "community": "Community - provide support, share your work, and learn together",
+        "search-ifc-class": "Search IFC class - find the correct IFC class to use in your BIM model",
     },
 }
 
@@ -126,7 +128,9 @@ environment = Environment(loader=FileSystemLoader("templates/"))
 
 for brand, content in pages.items():
     os.makedirs(f"{brand}_org_static_html", exist_ok=True)
-    shutil.copytree("assets", f"{brand}_org_static_html/assets",dirs_exist_ok=True)
+    shutil.copytree("assets", f"{brand}_org_static_html/assets", dirs_exist_ok=True)
+    if brand == "blenderbim":
+        shutil.copytree("blenderbim", f"{brand}_org_static_html", dirs_exist_ok=True)
     for page, title in content.items():
         template = environment.get_template(f"{page}.html")
         filename = f"{page}.html"
